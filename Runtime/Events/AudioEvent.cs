8e192ebdf3db549eba31973309988a3a96e5e8bb
amirardroudi <a.ardroudi@gmail.com>
2021-06-13 09:03:10 +0430

    update editor scripts

--- Runtime/Events/AudioEvent.cs
+++ Runtime/Events/AudioEvent.cs
@@ -1,9 +1,45 @@
 ﻿using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace BirdTools
 {
     public abstract class AudioEvent : ScriptableObject
     {
         public abstract void Play(AudioSource source);
     }
+    
+#if UNITY_EDITOR
+    [CustomEditor(typeof(AudioEvent), true)]
+    public class AudioEventEditor : UnityEditor.Editor
+    {
+        [SerializeField] private AudioSource _previewer;
+
+        private void OnEnable()
+        {
+            _previewer = EditorUtility.CreateGameObjectWithHideFlags("audio preview",
+                    HideFlags.HideAndDontSave, typeof(AudioSource))
+                .GetComponent<AudioSource>();
+        }
+
+        private void OnDisable()
+        {
+            DestroyImmediate(_previewer.gameObject);
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+        
+            EditorGUI.BeginDisabledGroup(serializedObject.isEditingMultipleObjects);
+            if (GUILayout.Button("preview"))
+            {
+                ((AudioEvent) target).Play(_previewer);
+            }
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+#endif
 }
--- Runtime/Events/GameEvent.cs
+++ Runtime/Events/GameEvent.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace BirdTools
 {
     [CreateAssetMenu(fileName = "ev_title", menuName = "BirdTools/Events/voidEvent")]
@@ -30,4 +34,25 @@ namespace BirdTools
                 _listeners.Add(listener);
         }
     }
+
+#if UNITY_EDITOR
+    
+    [CustomEditor(typeof(GameEvent), editorForChildClasses: true)]
+    public class GameEventEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            
+            base.OnInspectorGUI();
+
+            GUI.enabled = true;
+            
+            GameEvent e = target as GameEvent;
+            
+            if(GUILayout.Button("Raise Event"))
+                e.Raise();
+        }
+    }
+    
+#endif
 }
