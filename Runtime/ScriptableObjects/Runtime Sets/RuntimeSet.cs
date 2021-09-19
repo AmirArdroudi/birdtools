8cf0c7e0ebefb8fd76d93842eac458f870a3ef74
AmirArdroudi <a.ardroudi@gmail.com>
2021-09-19 17:23:25 +0300

    RuntimeSet Scriptable-Object created

---
+++ Runtime/ScriptableObjects/Runtime Sets/RuntimeSet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BirdTools
+{
+    public abstract class RuntimeSet<T> : ScriptableObject
+    {
+        public List<T> Items = new List<T>();
+
+        public void Add(T t)
+        {
+            if(!Items.Contains(t)) Items.Add(t);
+        }
+
+        public void Remove(T t)
+        {
+            if (Items.Contains(t)) Items.Remove(t);
+        }
+
+    }
+}
