d3c133a24b0c2e230485b5ea1e97cee470d46d8e
AmirArdroudi <a.ardroudi@gmail.com>
2021-10-19 21:07:15 +0300

    onTrigger component added

---
+++ Runtime/OnTrigger/OnTrigger.cs
@@ -0,0 +1,41 @@
+﻿using MyBox;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace VOID.GE
+{
+    [RequireComponent(typeof(Collider))]
+    public class OnTrigger : MonoBehaviour
+    {
+        [Tag]
+        public string colliderTag;
+        [Space]
+        public UnityEvent onEnter;
+        public UnityEvent onStay;
+        public UnityEvent onExit;
+
+        private void OnValidate()
+        {
+            GetComponent<Collider>().isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.gameObject.CompareTag(colliderTag)) return;
+            onEnter.Invoke();
+        }
+    
+        private void OnTriggerStay(Collider other)
+        {
+            if(!other.gameObject.CompareTag(colliderTag)) return;
+            onStay.Invoke();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if(!other.gameObject.CompareTag(colliderTag)) return;
+            onExit.Invoke();
+
+        }
+    }
+}
---
+++ Runtime/OnTrigger/OnTrigger2D.cs
@@ -0,0 +1,41 @@
+﻿using MyBox;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace VOID.GE
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class OnTrigger2D : MonoBehaviour
+    {
+        [Tag]
+        public string colliderTag;
+        [Space]
+        public UnityEvent onEnter;
+        public UnityEvent onStay;
+        public UnityEvent onExit;
+
+        private void OnValidate()
+        {
+            GetComponent<Collider2D>().isTrigger = true;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.gameObject.CompareTag(colliderTag)) return;
+            onEnter.Invoke();
+        }
+    
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if(!other.gameObject.CompareTag(colliderTag)) return;
+            onStay.Invoke();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if(!other.gameObject.CompareTag(colliderTag)) return;
+            onExit.Invoke();
+
+        }
+    }
+}
